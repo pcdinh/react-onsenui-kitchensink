95888bbce93fa9ce9730d9c85153c0baf84252c9
Andreas Argelius <andreas@argeli.us>
2016-05-19 14:10:39 +0900

    Update dependencies

--- components/InfiniteScroll.js
+++ components/InfiniteScroll.js
@@ -37,7 +37,7 @@ class InfiniteScroll extends React.Component {
         <LazyList
           length={10000}
           renderRow={this.renderRow}
-          calculateItemHeight={() => ons.platform.isAndroid() ? 77 : 45}
+          calculateItemHeight={() => ons.platform.isAndroid() ? 48 : 44}
         />
       </Page>
     );
